eee3418b476c1c9fa3a0cf7a6b1425cf3b1e1f7b
Gregor Riegler <gregor.riegler@ebcont.com>
2019-12-19 12:06:23 +0100

    # parallel change - prepare VetsDTO do be used in gateway.

--- clinic/src/main/java/org/springframework/samples/petclinic/service/SameProcessVetGateway.java
+++ clinic/src/main/java/org/springframework/samples/petclinic/service/SameProcessVetGateway.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class SameProcessVetGateway {
@@ -16,4 +17,20 @@ public class SameProcessVetGateway {
     Collection<Vet> vetsGateway() {
         return this.vets.findAll();
     }
+
+    Collection<VetDto> vets2Gateway() {
+        return this.vets.findAll()
+            .stream()
+            .map(this::createVetDto)
+            .collect(Collectors.toList());
+    }
+
+    private VetDto createVetDto(Vet v) {
+        return new VetDto(
+            v.getFirstName(),
+            v.getLastName(),
+            v.getSpecialties().stream().map(s -> s.getName()).collect(Collectors.toList()
+            ));
+    }
+
 }
