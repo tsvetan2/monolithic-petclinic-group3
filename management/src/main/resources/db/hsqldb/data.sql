3aa48255dad2844e91ed0f2ca87ecbad02b4c7ee
Gregor Riegler <gregor.riegler@ebcont.com>
2020-01-28 14:03:19 +0100

    Use different data in second service to distinguish.

--- management/src/main/resources/db/hsqldb/data.sql
+++ management/src/main/resources/db/hsqldb/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO revenue_time VALUES (1, 1, '2013-01-01', 100);
 INSERT INTO revenue_time VALUES (2, 2, '2013-01-02', 100);
-INSERT INTO revenue_time VALUES (3, 3, '2013-01-03', 300);
-INSERT INTO revenue_time VALUES (4, 4, '2013-01-04', 300);
+INSERT INTO revenue_time VALUES (3, 3, '2013-01-03', 400);
+INSERT INTO revenue_time VALUES (4, 4, '2013-01-04', 737);
