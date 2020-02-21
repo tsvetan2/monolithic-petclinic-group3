29ff0e90ed51715e8196d6a6837f8f63dea98d00
Gregor Riegler <gregor.riegler@ebcont.com>
2020-02-21 15:00:49 +0100

    refactor: remove unused

--- monolith/src/main/java/org/springframework/samples/petclinic/vets/Vet.java
+++ monolith/src/main/java/org/springframework/samples/petclinic/vets/Vet.java
@@ -52,22 +52,9 @@ public class Vet extends Person {
         return this.specialties;
     }
 
-    protected void setSpecialtiesInternal(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
-
     public List<Specialty> getSpecialties() {
         List<Specialty> sortedSpecs = new ArrayList<>(getSpecialtiesInternal());
         PropertyComparator.sort(sortedSpecs, new MutableSortDefinition("name", true, true));
         return Collections.unmodifiableList(sortedSpecs);
     }
-
-    public int getNrOfSpecialties() {
-        return getSpecialtiesInternal().size();
-    }
-
-    public void addSpecialty(Specialty specialty) {
-        getSpecialtiesInternal().add(specialty);
-    }
-
 }
