c35e5ea6b0bf66024a588a9a0f369c5c15fc4903
Gregor Riegler <gregor.riegler@ebcont.com>
2020-02-23 19:15:35 +0100

    rename vets application to distinguish from monolith

--- vets/src/main/java/org/springframework/samples/petclinic/vets/VetsApplication.java
+++ vets/src/main/java/org/springframework/samples/petclinic/vets/VetsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(proxyBeanMethods = false)
-public class Application {
+public class VetsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(VetsApplication.class, args);
     }
 
 }
